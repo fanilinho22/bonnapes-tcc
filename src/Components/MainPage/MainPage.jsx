57fbd045b45b1846d896df23f19076025a8c3075
fanilinho22 <guaranidapalhoca@gmail.com>
2023-10-18 12:35:27 -0300

    x

--- src/Components/MainPage/MainPage.jsx
+++ src/Components/MainPage/MainPage.jsx
@@ -1,21 +1,37 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import "./mainPage.css"
 import Header from '../Header/Header';
 import Footer from '../Footer/Footer';
-import Logo from './logo-bon.png'
+
 
 
 function Page() {
 
     return (
 
-        <div className='Home'>
+        <div className='mainPage'>
             <Header />
             
-            <h1>Home</h1>
-            <img src={Logo} alt="logo" />
+            <div className='nav-bar'>
+                <div className='nav-bar-box'>
+                    <a><Link to="/home">Pesquisar receitas</Link></a>
+                </div>
+                <div className='nav-bar-box'>
+                    <a><Link to="/home">Receitas salvas</Link></a>
+                </div>
+                <div className='nav-bar-box'>
+                    <a><Link to="/home">Conversor de medidas</Link></a>
+                </div>
+                <div className='nav-bar-box'>
+                    <a><Link to="/home">Sugestão</Link></a>
+                </div>
+                <div className='nav-bar-box'>
+                    <a><Link to="/home">Baixar receitas</Link></a>
+                </div>
+            </div>
 
-            <Footer/>
+            <Footer />
         </div>
     );
 }
